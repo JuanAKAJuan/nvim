cecca097edbd3296a69ac5728e1f60af6bea7c96
Juan <juancnmireles@gmail.com>
2024-02-17 01:03:42 -0500

    Edit the colorscheme

--- init.lua
+++ init.lua
@@ -190,6 +190,7 @@ require('lazy').setup({
 
   {
     'sainnhe/gruvbox-material',
+    -- 'ellisonleao/gruvbox.nvim',
     priority = 1000,
     lazy = false,
     config = function()
@@ -197,8 +198,9 @@ require('lazy').setup({
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_float_style = "dim"
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_foreground = 'material'
       vim.cmd.colorscheme("gruvbox-material")
+      -- vim.cmd.colorscheme("gruvbox")
     end,
   },
 
