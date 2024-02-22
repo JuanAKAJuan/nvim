153513e8f47fbde861fa7e04539891b56ce704b2
Juan <juancnmireles@gmail.com>
2024-02-22 17:10:16 -0500

    Remove icons from NvimTree

--- lua/juanakajuan/plugins/colorscheme.lua
+++ lua/juanakajuan/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
-        vim.g.gruvbox_material_background = 'hard'
+        vim.g.gruvbox_material_background = 'medium'
         vim.g.gruvbox_material_better_performance = 1
         vim.g.gruvbox_material_float_style = 'dim'
         vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
