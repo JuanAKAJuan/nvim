09b11ac28048a869675a7b097a598cb16e291b2b
Juan <juancnmireles@gmail.com>
2024-04-04 22:27:13 -0400

    Add bufferline plugin and remove harpoon

---
+++ lua/juanakajuan/plugins/bufferline.lua
@@ -0,0 +1,28 @@
+return {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        vim.opt.termguicolors = true
+        require("bufferline").setup {
+            options = {
+            },
+            highlights = {
+                buffer_selected = { italic = false },
+                diagnostic_selected = { italic = false },
+                hint_selected = { italic = false },
+                pick_selected = { italic = false },
+                pick_visible = { italic = false },
+                pick = { italic = false },
+            },
+        }
+
+        vim.keymap.set("n", "gb", "<CMD>BufferLinePick<CR>")
+        vim.keymap.set("n", "<leader>ts", "<CMD>BufferLinePickClose<CR>")
+        vim.keymap.set("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>")
+        vim.keymap.set("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>")
+        vim.keymap.set("n", "]b", "<CMD>BufferLineMoveNext<CR>")
+        vim.keymap.set("n", "[b", "<CMD>BufferLineMovePrev<CR>")
+        vim.keymap.set("n", "gs", "<CMD>BufferLineSortByDirectory<CR>")
+    end,
+}
