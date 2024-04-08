aee1ebe918d0e3f260f42a321e262182fbf43af2
Juan <juancnmireles@gmail.com>
2024-04-08 18:51:34 -0400

    Change keybinds for bufferline plugin

--- lua/juanakajuan/plugins/bufferline.lua
+++ lua/juanakajuan/plugins/bufferline.lua
@@ -17,12 +17,12 @@ return {
             },
         }
 
-        vim.keymap.set("n", "gb", "<CMD>BufferLinePick<CR>")
-        vim.keymap.set("n", "<leader>ts", "<CMD>BufferLinePickClose<CR>")
+        vim.keymap.set("n", "bp", "<CMD>BufferLinePick<CR>")
+        vim.keymap.set("n", "<leader>bc", "<CMD>BufferLinePickClose<CR>")
         vim.keymap.set("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>")
         vim.keymap.set("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>")
         vim.keymap.set("n", "]b", "<CMD>BufferLineMoveNext<CR>")
         vim.keymap.set("n", "[b", "<CMD>BufferLineMovePrev<CR>")
-        vim.keymap.set("n", "gs", "<CMD>BufferLineSortByDirectory<CR>")
+        vim.keymap.set("n", "bs", "<CMD>BufferLineSortByDirectory<CR>")
     end,
 }
