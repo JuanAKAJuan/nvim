1b9703e8d95250534c8327d32e5c16fd39ea4bd6
Juan <juancnmireles@gmail.com>
2024-04-11 14:14:33 -0400

    Replace nvim-tree with oil.nvim

--- lua/juanakajuan/init.lua
+++ lua/juanakajuan/init.lua
@@ -1,6 +1,3 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require "juanakajuan.options"
 require "juanakajuan.keymaps"
 require "juanakajuan.lazy_init"
--- lua/juanakajuan/keymaps.lua
+++ lua/juanakajuan/keymaps.lua
@@ -4,14 +4,7 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>zm", "<cmd>ZenMode<CR>")
 vim.keymap.set("n", "<leader>cl", "<cmd>ClangdSwitchSourceHeader<CR>")
 
--- vim.keymap.set("n", "<C-a>", "<C-^>")
-
--- vim.keymap.set("n", "<C-h>", "<C-w>h")
--- vim.keymap.set("n", "<C-l>", "<C-w>l")
--- vim.keymap.set("n", "<C-j>", "<C-w>j")
--- vim.keymap.set("n", "<C-k>", "<C-w>k")
-
-vim.keymap.set("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
+-- vim.keymap.set("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
