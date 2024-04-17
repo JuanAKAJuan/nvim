b381bf1f6f5197e535fa703333d9b6f02e77993d
Juan <juancnmireles@gmail.com>
2024-04-16 22:55:37 -0400

    Add keymaps for quickfix list and find and replace

--- lua/juanakajuan/keymaps.lua
+++ lua/juanakajuan/keymaps.lua
@@ -1,8 +1,32 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<leader>zm", "<cmd>ZenMode<CR>")
-vim.keymap.set("n", "<leader>cl", "<cmd>ClangdSwitchSourceHeader<CR>")
+-- Quick fix list navigation
+vim.keymap.set("n", "<C-j>", "<CMD>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<CMD>cprev<CR>zz")
+
+-- Replace the word that I am currently on
+vim.keymap.set(
+    "n",
+    "<leader>s",
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Find and replace on current word" }
+)
+
+vim.keymap.set(
+    "x",
+    "<leader>p",
+    '"_dP',
+    { desc = "Paste over highlighted text and still have the stuff I pasted" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>zm",
+    "<CMD>ZenMode<CR>",
+    { desc = "Activate ZenMode" }
+)
+vim.keymap.set("n", "<leader>cl", "<CMD>ClangdSwitchSourceHeader<CR>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -11,20 +35,19 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("i", "<C-b>", "<ESC>^i")
 vim.keymap.set("i", "<C-e>", "<End>")
 
-vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
-vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+vim.keymap.set("n", "<C-h>", "<CMD> TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-l>", "<CMD> TmuxNavigateRight<CR>")
 
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
+vim.keymap.set("n", "<C-f>", "<CMD>silent !tmux neww tmux-sessionizer.sh<CR>")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
@@ -42,19 +65,19 @@ vim.keymap.set(
     { expr = true, silent = true }
 )
 
--- Diagnostic keymaps
-vim.keymap.set(
-    "n",
-    "[d",
-    vim.diagnostic.goto_prev,
-    { desc = "Go to previous diagnostic message" }
-)
-vim.keymap.set(
-    "n",
-    "]d",
-    vim.diagnostic.goto_next,
-    { desc = "Go to next diagnostic message" }
-)
+-- -- Diagnostic keymaps
+-- vim.keymap.set(
+--     "n",
+--     "[d",
+--     vim.diagnostic.goto_prev,
+--     { desc = "Go to previous diagnostic message" }
+-- )
+-- vim.keymap.set(
+--     "n",
+--     "]d",
+--     vim.diagnostic.goto_next,
+--     { desc = "Go to next diagnostic message" }
+-- )
 vim.keymap.set(
     "n",
     "<leader>e",
