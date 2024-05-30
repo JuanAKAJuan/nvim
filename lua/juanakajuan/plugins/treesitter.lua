96d0ddc8474f56b2686f17710a0f7d48073e96b6
Juan <juancnmireles@gmail.com>
2024-05-30 00:01:17 -0400

    Changes

--- lua/juanakajuan/plugins/treesitter.lua
+++ lua/juanakajuan/plugins/treesitter.lua
@@ -33,7 +33,7 @@ return {
                 ignore_install = {},
                 -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
                 modules = {},
-                highlight = { enable = false },
+                highlight = { enable = true },
                 indent = { enable = true },
                 incremental_selection = {
                     enable = true,
